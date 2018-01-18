40dfbe23716de91ebcafce3cd7596c6e45ba4b73
astazyx <astazyx1991@users.noreply.github.com>
2018-01-18 12:59:20 +0800

    Update bubble_plot.py

--- DV/bubble_plot.py
+++ DV/bubble_plot.py
@@ -1,22 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Define the number of values
+# 定义气泡个数
 num_vals = 40
 
-# Generate random values
+# 生成随机值
 x = np.random.rand(num_vals)
 y = np.random.rand(num_vals)
 
-# Define area for each bubble
-# Max radius is set to a specified value
+# 定义每个点的个数
+# 最大半径
 max_radius = 25
 area = np.pi * (max_radius * np.random.rand(num_vals)) ** 2  
 
-# Generate colors
+# 生成颜色
 colors = np.random.rand(num_vals)
 
-# Plot the points
+# 画出数据点
 plt.scatter(x, y, s=area, c=colors, alpha=1.0)
 
+#show
 plt.show()
